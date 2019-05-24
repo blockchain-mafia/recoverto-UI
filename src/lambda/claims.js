e526e1f00bb810537222d0f6aa687e0e8df9b709
Nicolas Wagner <contact@wagner-nicolas.com>
2019-05-24 21:36:41 +0300

    test lambda config

--- src/lambda/claims.js
+++ src/lambda/claims.js
@@ -7,7 +7,7 @@ const { AIRTABLE_API_KEY, AIRTABLE_BASE } = process.env
 // const AIRTABLE_BASE = ''
 
 // TODO: use a bot instead of a netlify function to avoid a DDOS attack
-exports.handler = async (event, context) => {
+export function handler(event, context, callback) {
   // Only allow POST
   if (event.httpMethod !== "POST")
     return { statusCode: 405, body: "Method Not Allowed" }
@@ -26,7 +26,7 @@ exports.handler = async (event, context) => {
   // TODO: check if the signature is valid
   // if valid send to airtable else return 401 UNAUTHORIZED
   try {
-    await base('Owners').select({
+    base('Owners').select({
       view: 'Grid view',
       filterByFormula: `{Address} = '${address}'` // FIXME: ${drizzleState.account}
     }).firstPage((err, records) => {
@@ -34,34 +34,32 @@ exports.handler = async (event, context) => {
       records.forEach(record => {
         console.log('record', record)
         base('Claims').create({
-            "Address Finder": "send by prod 42", // FIXME: finder address
-            "Item ID": "test Prod", // FIXME: item ID
-            "Email Owner": "wagner.nicolas1@gmail.com", // idem
-            "Phone Number Owner": "+33650334223" // idem
-          }, 
-          err => { if (err) { console.log(err); return }
+          "Address Finder": "send by prod 42", // FIXME: finder address
+          "Item ID": "test Prod", // FIXME: item ID
+          "Email Owner": "wagner.nicolas1@gmail.com", // idem
+          "Phone Number Owner": "+33650334223" // idem
+        })
+        console.log(record);
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(record)
         })
       })
     })
-  } catch (error) { console.log(error)}
+  } catch (err) { 
+    console.log(err)
+    callback(null, {
+      statusCode: err.response.status,
+      body: JSON.stringify({ ...err.response.data })
+    });
+  }
 
   base('Claims').create({
-    "Address Finder": "send netlify", // FIXME: finder address
+    "Address Finder": "send netlify ewfewrgf", // FIXME: finder address
     "Item ID": "test Prod", // FIXME: item ID
     "Email Owner": "wagner.nicolas1@gmail.com", // idem
     "Phone Number Owner": "+33650334223" // idem
   }, 
-  err => { if (err) { console.log(err); return }
-})
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ 
-      itemID: "Hello, World!",
-      addressFinder: "",
-      addressOwner: "",
-      mailOwner: "wdqdq",
-      phoneNumberOwner: ""
-    })
-  }
+    err => { if (err) { console.log(err); return }
+  })
 }
